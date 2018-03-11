15db5e17a0db6f88f9ae3fa3ff99c19d93ab476a
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-11 17:53:52 +0000

    Add project files.

---
+++ SignalRExample/Scripts/SignalR.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    //Open the connection to the hub
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Hub Connected");
+            $.connection.myHub.server.announce("Pedro");
+        })
+        .fail(function () {
+            alert("Failed fucker");
+        });
+
+    $.connection.myHub.client.announce = function (msg) {
+        $("#welcome").append(msg);
+    }
+
+
+})()
+
+
+
---
+++ SignalRExample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRExample.Startup))]
+namespace SignalRExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
